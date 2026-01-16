4ea91174133d641d2d5740d5ae5d59aeda5927ec
asettar <achrafsettar8@gmail.com>
2026-01-16 11:33:00 +0100

    add database interface

--- src/models/Member.php
+++ src/models/Member.php
@@ -11,11 +11,12 @@ abstract class Member
     protected string    $email;
     protected string    $phoneNumber;
     protected ?DateTime $membershipEndDate = null;
+    protected int       $currentBorrowedCount; 
     protected int       $loanPeriod;
     protected float     $lateFee;
     protected int       $borrowLimit;
 
-    public function __construct(int $id, string $name, string $email, string $phoneNumber, ?DateTime $membershipEndDate) {
+    public function __construct(int $id, string $name, string $email, string $phoneNumber, ?DateTime $membershipEndDate, ) {
         $this->id = $id;
         $this->name = $name;
         $this->email = $email;
@@ -54,6 +55,16 @@ abstract class Member
     public function hasActiveMembership(): bool {
         $currentDate = new DateTime();
         return ($this->membershipEndDate && $currentDate <= $this->membershipEndDate);
+    }
+    
+    public function canBorrow() : bool {
+        if ($this->hasActiveMembership()) return true;
+        // todo later : check currentBorrowedCount against limit 
+        return false;
+    }
+
+    public function returnBook() {
+        // to do :
     }
 
     abstract public function renewMembership();
