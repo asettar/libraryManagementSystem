cc2982fd98074102b95d829fc957d4dfb04d891f
asettar <achrafsettar8@gmail.com>
2026-01-16 11:37:36 +0100

    add mySql connection class

---
+++ src/repositories/mySqlConnection.php
@@ -0,0 +1,29 @@
+<?php 
+
+use interfaces\databaseInterface;
+
+class mySqlConnection implements interfaces\databaseInterface {
+    private PDO $connection;
+
+    private string $host = "localhost";
+    private string $dbname = "librarySystem";
+    private string $username = "root";
+    private string $password = "AchrafSettar123*/";
+
+    public function __construct()
+    {
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8";
+            $this->connection = new PDO($dsn, $this->username, $this->password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+    public  function getConnection() {
+        return $this->connection;
+    }
+}
+
+    
+?>
