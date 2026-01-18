7af30f0b9b1714420f6bf198283245a1fd1ca28b
asettar <achrafsettar8@gmail.com>
2026-01-18 13:53:53 +0100

    add borrowRecord Model

--- src/models/BorrowRecord.php
+++ src/models/BorrowRecord.php
@@ -1,10 +1,45 @@
 <?php 
-class borrowRecord {
-    // borrowDate
-    // due date(based on member)
-    // actual returned date;
-    // bookIsbn
-    // member_id
-}
+namespace src\models;
+use DateTime;
 
+class BorrowRecord {
+    private string $bookIsbn;
+    private int $memberId;
+    private DateTime $borrowDate;
+    private DateTime $dueDate;
+
+    public function __construct(string $bookIsbn, int $memberId, DateTime $borrowDate, DateTime $dueDate) {
+        $this->bookIsbn = $bookIsbn;
+        $this->memberId = $memberId;
+        $this->borrowDate = $borrowDate;
+        $this->dueDate = $dueDate;
+    }
+
+    public function getBookIsbn(): string {
+        return $this->bookIsbn;
+    }
+
+    public function getMemberId(): int {
+        return $this->memberId;
+    }
+
+    public function getBorrowDate(): DateTime {
+        return $this->borrowDate;
+    }
+
+    public function getDueDate(): DateTime {
+        return $this->dueDate;
+    }
+
+    public function isOverdue(): bool {
+        $currentDate = new DateTime();
+        return $currentDate > $this->dueDate;
+    }
+
+    public function __toString(): string {
+        return "Book ISBN: {$this->bookIsbn}, Member ID: {$this->memberId}, "
+             . "Borrowed On: {$this->borrowDate->format('Y-m-d')}, "
+             . "Due On: {$this->dueDate->format('Y-m-d')}";
+    }
+}
 ?>
