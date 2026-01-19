aad103fa3db34cee274ed5c688d4a2e425fe0e76
asettar <achrafsettar8@gmail.com>
2026-01-19 21:22:13 +0100

    add tests for succefull borrow

--- tests/borrowSreviceTests.php
+++ tests/borrowSreviceTests.php
@@ -6,6 +6,8 @@ use src\repositories\{BookRepository, MemberRepository, BorrowRepository, Branch
 use src\repositories\mySqlConnection;
 use src\services\{BorrowService};
 use PHPUnit\Framework\TestCase;
+use src\factories\BorrowRecordFactory;
+use src\models\BorrowRecord;
 
 class borrowSreviceTests extends TestCase {
     private $db;
@@ -22,8 +24,13 @@ class borrowSreviceTests extends TestCase {
         $this->branchRepo = new BranchRepository($this->db);
         $this->borrowRepo = new borrowRepository($this->db);
         $this->borrowService = new BorrowService($this->memberRepo, $this->borrowRepo, $this->bookRepo, $this->branchRepo);
+        $this->db->beginTransaction();
     }
 
+    public function tearDown(): void {
+        $this->db->rollBack();
+    }
+    
     public function testBookNotFoundException() {
         $this->expectException(Exception::class);
         $this->expectExceptionMessage("Book with isbn: 234234 not found.");
@@ -41,11 +48,35 @@ class borrowSreviceTests extends TestCase {
         $this->expectExceptionMessage("Member with id : 2343 not found.");
         $this->borrowService->borrowBook(2343, 2, '9780262033851');
     }
-
-    public function testMemberNotAvailable() {
-
+    
+    public function testMemberNotAvailableException() {
+        // unpaid fees > 10 for member with id = 2;
+        
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("Member with id : 2 is not available for borrowing.");
+        $this->borrowService->borrowBook(2, 1, '9780131103628');
     }
 
-}
+    public function testBookUnavailableException() {
+        // book status != available
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("Book with isbn : 9780131103627 is currently not available.");
+        $this->borrowService->borrowBook(1, 2, '9780131103627');
+    }
 
+    public function testSuccessfullBorrow() {
+        $prevMember =  $this->memberRepo->findById(3);
+        $this->borrowService->borrowBook(3, 1, '9780131103628');
+        $this->borrowRepo->find('9780131103628', 3);  // no exception should be thrown
+        
+        $book = $this->bookRepo->findByISBN('9780131103628'); 
+        $member = $this->memberRepo->findById(3);
+        $this->assertEquals($book->getStatus(), 'checked_out');
+        // echo ($book);
+        // echo $member;
+        // echo $prevMember;
+        $this->assertEquals($member->getCurrentBorrowedCount(), $prevMember->getCurrentBorrowedCount() + 1);
+        // BorrowRecord
+    }
+}
 ?>
