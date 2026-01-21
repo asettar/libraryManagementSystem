2727c2d7f93bb21bf82cdd316742c959aba19a36
asettar <achrafsettar8@gmail.com>
2026-01-21 16:10:28 +0100

    add renewMemership method to ManagementService class

--- src/services/managementService.php
+++ src/services/managementService.php
@@ -1,15 +1,38 @@
 <?php 
 
-// class managementSerivce {
-//     memberRepo
-    // BorrowRepo
-//     regiterNewMember(memberid);
-//     renewMembership(memberID);
-//     updateContact(memberId);
-//      payFees(memberId) 
-        // viewBorrowHistory(memberId);
+namespace src\services;
 
-// }
+use src\models\{Member};
+use src\repositories\{MemberRepository};
+use DateTime;
+use Exception;
+
+class ManagementService {
+    private MemberRepository    $memberRepo;
+    
+    public function __construct(MemberRepository $memberRepo) {
+        $this->memberRepo = $memberRepo;
+    }
+    
+    // renewMembership(memberID);
+    // updateContact(memberId);
+    //  payFees(memberId) 
+
+    public function renewMembership(int $memberId) : void {
+        $member = $this->memberRepo->findById($memberId);
+        $member->renewMembership();
+        $this->memberRepo->update($member);
+    }
+
+    public function updateContact(int $memberId) : void {
+
+    }
+
+    public function regiterNewMember() : void {
+
+    }
+
+}
 
 // libraryService {
 //     managementService
